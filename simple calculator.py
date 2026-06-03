23cbe14fb3a3cbc56f7072fc436ad31892106222
VivekYadav2186047-cmd <vy2186047@gmail.com>
2026-06-03 17:53:59 +0530

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,20 @@
+'''Q6: Simple Calculator
+Write a program that:
+
+Takes two numbers as input from the user.
+
+Prints their:
+
+Sum
+Difference
+Product
+Quotient'''
+
+a = input("enter the first number =")
+b = input("enter the second number =")
+a = int(a)
+b = int(b)
+print("a + b is" , a + b)
+print("a - b is" , a - b)
+print("a * b is" , a * b)
+print("a % b is" , a % b)
