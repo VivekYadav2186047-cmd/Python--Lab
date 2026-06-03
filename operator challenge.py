c82cbf71c395b5d2d46524ba9d60803e37a2ccc6
VivekYadav2186047-cmd <vy2186047@gmail.com>
2026-06-03 17:54:33 +0530

    Add files via upload

---
+++ operator challenge.py
@@ -0,0 +1,11 @@
+'''Q8: Operator Challenge
+Write a program that:"
+
+Takes an integer as input from the user.
+Prints the square and cube of that number.'''
+a = int(input("enter the number = "))
+# print("square of that number" , a*a)
+# print("cube of that number" , a*a*a)
+
+print("square is" , a**2)
+print("cube is" , a**3)
