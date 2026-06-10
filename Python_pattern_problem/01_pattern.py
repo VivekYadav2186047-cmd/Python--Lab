e652b66511ce617d3d86a78837d7ff34da96d3de
VivekYadav2186047-cmd <vy2186047@gmail.com>
2026-06-10 19:58:26 +0530

    Correct pattern printing logic in 01_pattern.py
    Fix indentation and loop increment errors in pattern printing.

---
+++ Python_pattern_problem/01_pattern.py
@@ -0,0 +1,8 @@
+i = 1
+while(i <= 5):
+    j = 1
+    while( j <= i):
+        print("*" , end="") #using end to not move into the next lime
+        j+=1
+    print()#to move in to the next line
+    i+=1
