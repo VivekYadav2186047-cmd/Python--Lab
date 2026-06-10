10240757b6a16670b3294e31ca8ec96172a425c2
VivekYadav2186047-cmd <vy2186047@gmail.com>
2026-06-10 18:10:47 +0530

    Delete even_odd_check.py

--- Loops , break,continue /even_odd_check.py
+++
@@ -1,8 +0,0 @@
-'''Write a program that takes a number from the user and prints "Even" if it is even, otherwise "Odd".
-'''
-n = int(input("enter the number\n = "))
-
-if(n % 2== 0):
-    print("number is even")
-else:
-    print("number is odd")
