41d174a9e1a0654a23da99c07119cab93fff426b
VivekYadav2186047-cmd <vy2186047@gmail.com>
2026-06-10 20:50:40 +0530

    Add math functions for square root and sine calculation

---
+++ Function_Recursion_Python/function_module.py
@@ -0,0 +1,11 @@
+#Import the math module and use it to:
+
+# Find the square root of 144
+# Calculate sin(90°) (hint: use math.radians())
+import math 
+
+print(math.sqrt(144))
+
+print(math.sin(math.radians(90)))
+
+
