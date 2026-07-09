5ff6f7afe76f66dbe15360c906f424198e92bbf5
VivekYadav2186047-cmd <vy2186047@gmail.com>
2026-07-10 01:30:12 +0530

    Add number comparison script
    Implement a program to compare two numbers and print the larger one.

---
+++ Conditionals_python/number_comparison.py
@@ -0,0 +1,11 @@
+# Largest of Two Numbers
+
+# Input two numbers.
+# Print the larger number.
+a = int(input("enter the number a :"))
+b = int(input("enter the number b :"))
+if (a > b):
+    print( a , "is largest")
+
+else:
+    print(b , "is largest")
